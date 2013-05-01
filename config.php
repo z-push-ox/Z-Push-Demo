b677b92b8e77bc58dea892a30e7506eae39d4981
liob <hbwinther@metalabs.de>
2013-05-01 22:48:36 +0200

    .

--- config.php
+++ config.php
@@ -108,7 +108,7 @@
  *  Mobile settings
  */
     // Device Provisioning
-    define('PROVISIONING', true);
+    define('PROVISIONING', false);
 
     // This option allows the 'loose enforcement' of the provisioning policies for older
     // devices which don't support provisioning (like WM 5 and HTC Android Mail) - dw2412 contribution
